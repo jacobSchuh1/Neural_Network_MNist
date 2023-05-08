203fb8ef0beff697d8035648e1792c675aac18ad
jacobSchuh1 <114092120+jacobSchuh1@users.noreply.github.com>
2023-05-07 20:07:01 -0400

    Add files via upload
    Python files
    ReadMe

---
+++ Training.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+#establishes the weights for the Neural Net
+def init_params():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+    return W1, b1, W2, b2
---
+++ main.py
@@ -0,0 +1,55 @@
+from Training import *
+from AIBot import *
+
+#Import and set the data
+
+data = pd.read_csv('train.csv')
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+
+
+#Run the Program
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500, m)
+#0.8502439024390244
+
+def predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+test_prediction(0, W1, b1, W2, b2)
+test_prediction(1, W1, b1, W2, b2)
+test_prediction(2, W1, b1, W2, b2)
+test_prediction(3, W1, b1, W2, b2)
+
+dev_predictions = predictions(X_dev, W1, b1, W2, b2)
+acc = get_accuracy(dev_predictions, Y_dev)
+
+print(acc)
